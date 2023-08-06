bc1c9add4e5766ca880e3c2cb88b48a08702cf68
ytakahashi <yt01tc@gmail.com>
2023-08-07 08:48:48 +0900

    feat: update igit tag (#25)

--- commands/tag.zsh
+++ commands/tag.zsh
@@ -2,8 +2,8 @@ _igit_tag(){
     local tag
     while tag=$(
         git tag |
-        _fzf_for_igit +m --expect=ctrl-s,alt-p,alt-s \
-        --header "ctrl-s: show the tag, alt-p: push the tag to origin, alt-s: switch" \
+        _fzf_for_igit -m --expect=alt-c,ctrl-s,alt-c,alt-p,alt-s \
+        --header "ctrl-s: show the tag, alt-c show diff between the tags, alt-p: push the tag to origin, alt-s: switch" \
         --preview 'git show --color=always {}'); do
 
         if [[ -z $tag ]]; then
@@ -19,6 +19,11 @@ _igit_tag(){
 
         if [ $cmd = ctrl-s ]; then
             git show --color=always $t | less -R
+        elif [ $cmd = alt-c ]; then
+            local diff=(`awk 'NR==2{printf $1};NR==3{printf "...%s",$1}' <<< "$tag"`)
+            echo "git log $diff --pretty=format:'- %s (%h)'"
+            git log $diff --pretty=format:'- %s (%h)'
+            break
         elif [ $cmd = alt-p ]; then
             print -z "git push origin $t"
             break
